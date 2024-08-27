283f42e15848d74d4ab851bd30ff0261828cda3d
Jonnthan3visan <joptrabajo@hotmail.com>
2024-08-27 20:00:56 -0300

    principal
    botones funcionando todos, reenvia los link correctamente

--- javascript/app.js
+++ javascript/app.js
@@ -1,22 +1,58 @@
 function cambiarTextoBotonIniciar() {
-    let TextoBoton= document.getElementById("boton-iniciar");
-   TextoBoton.value = "Iniciando"
+    let TextoBoton = document.getElementById("boton-iniciar");
+    TextoBoton.value = "Iniciando"
 }
-
-
 
 function volverPaginaPrincipal() {
-     document.getElementById("boton-incio").addEventListener('click',function(){
-        window.location.href="../html/principal.html"
+    document.getElementById("boton-incio").addEventListener('click', function () {
+        window.location.href = "../html/principal.html"
+    })
+}
+
+function redirijeClientes() {
+    document.getElementById("boton-clientes").addEventListener("click", function () {
+        window.location.href = "../html/mostrar-clientes.html"
+    })
+}
+
+function paginaNuevoCliente() {
+    document.getElementById("boton-nuevo-cliente").addEventListener("click", function() {
+        window.location.href="../html/nuevo-cliente.html"
+    })
+}
+
+function paginaModificarCliente() {
+    document.getElementById("boton-modificar-cliente").addEventListener("click",function(){
+        window.location.href="../html/modificar-cliente.html"
+    })
+
+}
+
+function redirijeEliminarCliente() {
+    document.getElementById("boton-eliminar-cliente").addEventListener('click', function () {
+        window.location.href = "../html/eliminar-clientes.html"
+    })
+}
+
+function paginaTrabajosPendiente() {
+    document.getElementById("boton-trabajos-pendientes").addEventListener("click",function(){
+        window.location.href="../html/trabajos-pendientes.html"
+    })
+    
+}
+
+
+function cerrarSesion() {
+    document.getElementById("boton-cerrar-sesion").addEventListener('click', function () {
+        window.location.href = "..//html/index.html"
     })
 }
 
 
-function cerrarSesion (){
-    document.getElementById("boton-cerrar-sesion").addEventListener('click', function(){
-        window.location.href="..//html/index.html"
-    })
-}
+
+
+
+
 
 /* 
 
---
+++ javascript/calendario.js
@@ -0,0 +1,7 @@
+//  <link href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.2/main.min.css" rel="stylesheet">
+
+/* 
+#calendar {
+    max-width: 900px;
+    margin: 0 auto;
+} */
