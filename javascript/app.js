2a949c356dcba1946cb7e729afd0d9b4cd8b9693
Jonnthan3visan <joptrabajo@hotmail.com>
2024-08-27 19:06:12 -0300

    principal
     boton cerrar sesion y inicio cumplen con su funcion

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,24 @@
 function cambiarTextoBotonIniciar() {
     let TextoBoton= document.getElementById("boton-iniciar");
-
    TextoBoton.value = "Iniciando"
 }
+
+
+
+function volverPaginaPrincipal() {
+     document.getElementById("boton-incio").addEventListener('click',function(){
+        window.location.href="../html/principal.html"
+    })
+}
+
+
+function cerrarSesion (){
+    document.getElementById("boton-cerrar-sesion").addEventListener('click', function(){
+        window.location.href="..//html/index.html"
+    })
+}
+
+/* 
+
+<a href="../html/principal.html" id="boton-incio" type="button" class="btn btn-outline-dark">Inicio</a>
+<a href="../html/principal.html" id="boton-incio" type="button" class="btn btn-outline-dark">Inicio</a> */
